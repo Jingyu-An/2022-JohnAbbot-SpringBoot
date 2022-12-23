cf095ea8944e6995fdc14cc2d39b7c855f90b21e
Jingyu-An <jqrohan@gmail.com>
2022-12-23 12:14:18 -0500

    [MongoDB] Add post API

--- MongoDb/src/main/java/com/example/mongodb/controller/UserController.java
+++ MongoDb/src/main/java/com/example/mongodb/controller/UserController.java
@@ -9,14 +9,19 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/users")
 public class UserController {
 
 	@Autowired
 	UserRepository userRepo;
 
-	@GetMapping("/users")
-	public List<User> findAllUsers() {
+	@GetMapping("/")
+	List<User> findAllUsers() {
 		return userRepo.findAll();
+	}
+
+	@PostMapping("/")
+	User saveUser(@RequestBody User user) {
+		return userRepo.save(user);
 	}
 }
